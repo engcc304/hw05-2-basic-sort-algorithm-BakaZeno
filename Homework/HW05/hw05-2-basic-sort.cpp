c82c93c6ffc78109f3a940dd6e4540d426293167
BakaZeno <agarz3no@gmail.com>
2023-08-16 16:25:03 +0700

    test

--- Homework/HW05/hw05-2-basic-sort.cpp
+++ Homework/HW05/hw05-2-basic-sort.cpp
@@ -21,3 +21,32 @@
     Output:
         112 91 65
 */
+#include <stdio.h>
+int main(){
+    int num1,num2,num3;
+    printf("Input[1]\n");
+    scanf("%d",&num1);
+    printf("Input[2]\n");
+    scanf("%d",&num2);
+    printf("Input[3]\n");
+    scanf("%d",&num3);
+    if (num1 >= num2 && num1 >= num3) {
+        if (num2 >= num3) {
+            printf("%d %d %d\n", num1, num2, num3);
+        } else {
+            printf("%d %d %d\n", num1, num3, num2);
+        }
+    } else if (num2 >= num1 && num2 >= num3) {
+        if (num1 >= num3) {
+            printf("%d %d %d\n", num2, num1, num3);
+        } else {
+            printf("%d %d %d\n", num2, num3, num1);
+        }
+    } else {
+        if (num1 >= num2) {
+            printf("%d %d %d\n", num3, num1, num2);
+        } else {
+            printf("%d %d %d\n", num3, num2, num1);
+        }
+    }
+}
